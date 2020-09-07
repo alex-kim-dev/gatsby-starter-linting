0005ec891508044c49096f9362e458f002ff7861
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2020-09-07 14:25:11 +0500

    Reword info on this starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby starter`,
+    description: `A starter with a single hello world component and some code quality checking tools`,
+    author: `Alex Kim`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
