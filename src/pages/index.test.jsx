a6949da1d68b68d1b4046b9423ac592cb8eb0810
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-05-30 11:11:00 +0500

    add react testing library
    remove react-test-renderer

--- src/pages/index.test.jsx
+++ src/pages/index.test.jsx
@@ -1,13 +1,13 @@
+import '@testing-library/jest-dom/extend-expect';
+
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import renderer from 'react-test-renderer';
 
 import HomePage from './index';
 
 describe('Home page', () => {
   it('renders correctly', () => {
-    const tree = renderer
-      .create(<HomePage siteTitle='Default Starter' />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+    render(<HomePage />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
   });
 });
